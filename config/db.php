646222099268b756a8b018c97e2a05f51df2d6ec
LGugs <lgpa@icomp.ufam.edu.br>
2017-03-15 01:34:40 -0400

    config bd

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=dbutnc00549ktd', // configurado para receber o server do heroku
+    'dsn' => 'pgsql:host=ec2-23-21-186-138.compute-1.amazonaws.com;port=5432;dbname=dbutnc00549ktd', // configurado para receber o server do heroku
     'username' => 'rswdgzbwupxjxh',
     'password' => 'a757e07d7325913e84e475974e27e681ee57e649c0642d583ffda21f520fa096',
     'charset' => 'utf8',
