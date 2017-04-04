3a77667efddae03190c2b46a3bf52c18a18e268d
LGugs <lgpa@icomp.ufam.edu.br>
2017-04-04 00:01:30 -0400

    atualização do db

--- config/db.php
+++ config/db.php
@@ -2,8 +2,18 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=ec2-54-225-236-102.compute-1.amazonaws.com;port=5432;dbname=d30q79iaor1jm0', // configurado para receber o server do heroku
-    'username' => 'itmbpyzwkzbcpg',
-    'password' => '2c18c0a843f3da6e199c865a130a5528426b8ea528cc7368e2c4e4cce06aeb11',
+    'dsn' => 'pgsql:host=ec2-54-225-118-55.compute-1.amazonaws.com;port=5432;dbname=da4sfajgi75tnj', // configurado para receber o server do heroku
+    'username' => 'xtdlbmytgxusqw',
+    'password' => '26d2d232a902a137eb702244a5aeb6111a207db76f7dc9f100787440deff88c4',
     'charset' => 'utf8',
 ];
+
+/*
+return [
+		'class' => 'yii\db\Connection',
+		'dsn' => 'pgsql:host=localhost;port=5432;dbname=amazoncompanies', // configurado para receber o server do heroku
+		'username' => 'postgres',
+		'password' => 'ap310608039090lg',
+		'charset' => 'utf8',
+];
+*/
