e987cb49915bb5c3af533d8ca19cf42a719e9c44
LGugs <lgpa@icomp.ufam.edu.br>
2017-05-10 13:17:17 -0400

    heroku novo banco

--- config/db.php
+++ config/db.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=ec2-184-73-199-72.compute-1.amazonaws.com;port=5432;dbname=d3uuhsvruseimi', // configurado para receber o server do heroku
-    'username' => 'pczogfowrrduvj',
-    'password' => '2770dbf52f3fa8d2db2fdcddf2b5c300436b1b42919f113ddea48395e4cbd2cd',
+    'dsn' => 'pgsql:host=ec2-50-19-89-124.compute-1.amazonaws.com;port=5432;dbname=dc5f83s21o4hch', // configurado para receber o server do heroku
+    'username' => 'jmzmreezuvuphl',
+    'password' => 'ceb4ebdeafb1025d51a617d4022601a7e4c88d57a8156876050ab2b194a12464',
     'charset' => 'utf8',
 ];
-
 /*
+
 return [
 		'class' => 'yii\db\Connection',
 		'dsn' => 'pgsql:host=localhost;port=5432;dbname=amazoncompanies', // configurado para receber o server do heroku
