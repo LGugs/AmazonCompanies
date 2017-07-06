19495305dac45769f7901ea1694b1ded9a347adb
LGugs <lgpa@icomp.ufam.edu.br>
2017-07-05 23:17:26 -0400

    att db

--- config/db.php
+++ config/db.php
@@ -2,18 +2,18 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=ec2-50-19-89-124.compute-1.amazonaws.com;port=5432;dbname=dc5f83s21o4hch', // configurado para receber o server do heroku
-    'username' => 'jmzmreezuvuphl',
-    'password' => 'ceb4ebdeafb1025d51a617d4022601a7e4c88d57a8156876050ab2b194a12464',
+    'dsn' => 'pgsql:host=ec2-23-21-96-159.compute-1.amazonaws.com;port=5432;dbname=d5lefdlrp55r7o', // configurado para receber o server do heroku
+    'username' => 'fvufmgqaafvddh',
+    'password' => 'd5abd67210285bb3d88eaca5ce209f387f36a9e76b6ceac421090d1a4795a655',
     'charset' => 'utf8',
 ];
-/*
 
+/*
 return [
 		'class' => 'yii\db\Connection',
 		'dsn' => 'pgsql:host=localhost;port=5432;dbname=amazoncompanies', // configurado para receber o server do heroku
 		'username' => 'postgres',
-		'password' => 'ap310608039090lg',
+		'password' => '',
 		'charset' => 'utf8',
 ];
 */
